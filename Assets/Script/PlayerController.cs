096596e25b8abb77fe98819621e6c88959c029b9
1447837654 <1447837654@qq.com>
2022-10-11 22:06:38 +0800

    解决玩家空中撞墙不会下落bug（添加物理材质） 解决无限跳跃bug 增加玩家获取樱桃和钻石数量UI 增加敌人，实现跳跃踩到敌人消灭敌人

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D playerRb; // 玩家刚体
     public float playerSpeed; // 玩家速度
     public float jumpForce; // 跳跃力
+    public TextMeshProUGUI cherryText; // 玩家获得的樱桃数量文本
+    public TextMeshProUGUI gemText; // 玩家获得的钻石数量文本
 
     private Animator playerAnimator; //玩家动画
     public Collider2D playerCollider; // 玩家碰撞体
@@ -15,6 +18,8 @@ public class PlayerController : MonoBehaviour
     private Vector2 playerVelocity; // 玩家速度
     private float playerFacedDire; // 玩家朝向
     private Vector3 playerScale; // 控制左右翻转
+    private int cherryNum; // 玩家获取的樱桃数量
+    private int gemNum; // 玩家获取的钻石数量
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +29,11 @@ public class PlayerController : MonoBehaviour
 
         playerVelocity = new Vector2(playerRb.velocity.x, playerRb.velocity.y);
         playerScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+        cherryNum = 0;
+        cherryText.text = "Cherry:" + cherryNum;
+        gemNum = 0;
+        gemText.text = "Gem:" + gemNum;
     }
 
     // Update is called once per frame
@@ -56,7 +66,8 @@ public class PlayerController : MonoBehaviour
         }
 
         // 玩家跳跃
-        if (Input.GetButtonDown("Jump") || Input.GetButton("Jump"))
+        if ((Input.GetButtonDown("Jump") || Input.GetButton("Jump"))
+            && playerCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
         {
             playerVelocity.x = playerRb.velocity.x;
             playerVelocity.y = jumpForce;
@@ -72,9 +83,16 @@ public class PlayerController : MonoBehaviour
             playerAnimator.SetBool("Falling", true);
         }
 
-        if (playerCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        // 玩家匍匐
+        if (Input.GetAxisRaw("Vertical") == -1f && playerCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            playerAnimator.SetBool("Crouching", true);
+            playerAnimator.SetBool("Idle", false);
+        }
+        else if (playerCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
         {
             playerAnimator.SetBool("Falling", false);
+            playerAnimator.SetBool("Crouching", false);
             playerAnimator.SetBool("Idle", true);
         }
     }
@@ -82,13 +100,34 @@ public class PlayerController : MonoBehaviour
     // 拾取物品碰撞检测
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision != null)
+        if (collision == null)
         {
             Debug.Log("碰撞体为NULL");
         }
 
         if (collision.CompareTag("Item"))
         {
+            if (collision.gameObject.name.Equals("Cherry"))
+            {
+                cherryNum++;
+                cherryText.text = "Cherry:" + cherryNum;
+            }
+            else if (collision.gameObject.name.Equals("Gem"))
+            {
+                gemNum++;
+                gemText.text = "Gem:" + gemNum;
+            }
+            Destroy(collision.gameObject);
+        }
+        else if (collision.CompareTag("Enemy") && playerAnimator.GetBool("Falling"))
+        {
+            playerVelocity.x = playerRb.velocity.x;
+            playerVelocity.y = jumpForce;
+            playerRb.velocity = playerVelocity;
+
+            playerAnimator.SetBool("Falling", false);
+            playerAnimator.SetBool("Jumping", true);
+
             Destroy(collision.gameObject);
         }
     }
